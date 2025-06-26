9f5a617f9d835f7858f7542c1b27cf7ce988a351
ynzung <syj91105@sookmyung.ac.kr>
2025-06-27 01:27:57 +0900

    [Feat] App 컴포넌트 기본 레이아웃 설정

--- first-semester/week07/src/App.jsx
+++ first-semester/week07/src/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 80px 100px;
+  width: 100%;
+  min-height: 100dvh;
+  background-color: var(--bg-grey);
+`;
+
+const Title = styled.div`
+  color: var(--text-black);
+  font-size: 50px;
+  font-weight: bolder;
+  cursor: pointer;
+`;
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <Outlet />
+      <Wrapper>
+        <Title onClick={() => navigate("/")}>숙멋 프론트의 TMI 게시판 🦁</Title>
+        <Outlet />
+      </Wrapper>
     </>
   );
 }

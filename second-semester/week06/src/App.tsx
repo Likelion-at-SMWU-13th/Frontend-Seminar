9b98b871618f336ef7346cfaf222f2a45fe0dde7
ynzung <syj91105@sookmyung.ac.kr>
2025-10-27 23:52:13 +0900

    [Feat] 2학기 6주차 세미나 실습

--- second-semester/week06/src/App.tsx
+++ second-semester/week06/src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <Title onClick={() => navigate("/")}>숙멋 프론트의 TMI 게시판 🦁</Title>
+      <Outlet />
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  padding: 5rem 6.25rem;
+  width: 100%;
+  min-height: 100dvh;
+  background-color: var(--bg-grey);
+`;
+
+const Title = styled.div`
+  color: var(--text-black);
+  font-size: 3.125rem;
+  font-weight: bolder;
+  cursor: pointer;
+`;
